2561a944c2e88c0efda88f4ea52e1d0f377f1fc2
אנונימי <me@somewhaere.au>
2016-06-02 11:08:29 +0300

    buityfy

--- wanted/wanted/settings.py
+++ wanted/wanted/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '06zyl3erpfrz=myi%2@g7&e-%-wyl^-3b^flmg4yw2w_^7=^f^'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,13 +73,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wanted.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-#postgres on virtualbox
-    'not_default': {
+    # postgres on virtualbox
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wanted',
         'USER': 'admin',
@@ -89,13 +86,12 @@ DATABASES = {
         'HOST': '192.168.7.59',
         'PORT': '5432',
 
-    } ,
-    'default': {
+    },
+    '_default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -115,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
@@ -128,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
